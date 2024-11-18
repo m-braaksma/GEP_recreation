d615e4ba03da7405d2547414ec1e6495fc45135d
Matt Braaksma <braaksma@protonmail.com>
2024-11-18 13:58:24 -0600

    troubleshoot unwto expense data and add cleaned csv

--- scripts/run_clean_unwto_expenses.py
+++ scripts/run_clean_unwto_expenses.py
@@ -3,44 +3,41 @@ Purpose: Clean the UNWTO tourism expenditure data.
 """
 
 # Dependencies 
-import os
 import numpy as np
 import pandas as pd
-import pycountry
-    # NOTE: You will need to install pycountry in miniforge 
+import country_converter as cc
+    # NOTE: You will need to install country_converter in miniforge 
     # conda activate teem_devstack
     # conda install pycountry
-
-# Label country codes
-def get_iso3_code(country_name):
-    try:
-        country = pycountry.countries.get(name=country_name)
-        if country:
-            return country.alpha_3
-        else:
-            return None
-    except KeyError:
-        return None
     
 # Function: Clean data set
 def fun_clean_expenses(df):
-    # Add Variable: ISO-3 Alpha country codes
-    df['iso_code'] = get_iso3_code(df['country'])
-    # Reshape (pivot table)
-    df = pd.melt(df, 
-                id_vars = ["c", "c_s", "country", "iso_code"], 
-                value_vars = [str(year) for year in range(1995, 2023)],  # Years 1995 to 2022
-                var_name = "year", 
-                value_name = "expense")
-    # Replace exact zero with missing
-    df["expense"].replace(0, np.nan, inplace=True)
-    # Rescale for million dollar units
-    df["expense"] = df["expense"] * 1e6
 
+    # Add Variable: ISO-3 Alpha country codes
+    df['iso3'] = cc.convert(df['country'],to='ISO3') 
+
+    # Ensure that columns representing years are integers
+    year_cols = [col for col in df.columns if isinstance(col, int) and 1995 <= col <= 2022]
+    
+    # Reshape (pivot table)
+    df_long = pd.melt(
+        df,
+        id_vars=["c", "c_s", "country", "iso3"],
+        value_vars=year_cols,
+        var_name="year",
+        value_name="expense"
+    )
+    
+    # Replace exact zeros with missing values
+    df_long["expense"].replace(0, np.nan, inplace=True)
+    
+    # Rescale for million dollar units
+    df_long["expense"] = df_long["expense"].astype(float) * 1e6
+    
+    return df_long
 
 # Bring in data 
 df_unwto_expenses = pd.read_excel("../data/unwto-inbound-tourism-expenditure.xlsx")
-    # TODO:  I can't figure out why the directory path is failing here...
 
 # Clean data 
 df_unwto_expenses = fun_clean_expenses(df_unwto_expenses)
